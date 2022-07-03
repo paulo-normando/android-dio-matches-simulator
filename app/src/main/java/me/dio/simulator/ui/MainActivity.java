1c412423fc5b0001ecd1fcdd3676b7ef4baca438
Paulo Normando <paulonormando@gmail.com>
2022-07-02 23:11:00 -0300

    15 commit

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -1,13 +1,33 @@
 package me.dio.simulator.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import me.dio.simulator.R;
+import me.dio.simulator.data.MatchesApi;
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.domain.Match;
+import me.dio.simulator.ui.adapter.MatchesAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
+    private RecyclerView.Adapter matchesAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,9 +36,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setupHttpClient();
+
         setupMatchesList();
         setupMatchesRefresh();
         setupFloatingActionButton();
+    }
+
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://paulo-normando.github.io/android-dio-matches-simulator-api/matches.json")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        matchesApi = retrofit.create(MatchesApi.class);
     }
 
     private void setupFloatingActionButton() {
@@ -30,6 +60,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupMatchesList() {
-    //TODO
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+        @Override
+        public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+            if (response.isSuccessful()) {
+                List<Match> matches = response.body();
+                matchesAdapter = new MatchesAdapter(matches);
+                binding.rvMatches.setAdapter(matchesAdapter);
+            } else {
+                showErrorMessages();
+            }
+        }
+
+        @Override
+        public void onFailure(Call<List<Match>> call, Throwable t) {
+            showErrorMessages();
+        }
+    });
+    }
+
+    private void showErrorMessages() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
 }
