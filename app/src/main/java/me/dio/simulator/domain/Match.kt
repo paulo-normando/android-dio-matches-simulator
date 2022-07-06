d997671937169a5b1513fcd4c1381cfa6054aa06
Paulo Normando <paulonormando@gmail.com>
2022-07-06 12:27:06 -0300

    22 commit

--- app/src/main/java/me/dio/simulator/domain/Match.kt
+++ app/src/main/java/me/dio/simulator/domain/Match.kt
@@ -1,18 +1,17 @@
 package me.dio.simulator.domain
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Match (
+data class Match(
     @SerializedName("descricao")
     val description: String,
     @SerializedName("local")
     val place: Place,
     @SerializedName("mandante")
     val homeTeam: Team,
-    @SerializedName("visitante ")
+    @SerializedName("visitante")
     val awayTeam: Team
 ) : Parcelable
